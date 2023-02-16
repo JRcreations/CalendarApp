4b7cc074fcaf85bed2a61c550af87808d1e22a75
John Reichel <jreichel447@gmail.com>
2023-02-16 09:09:03 -0600

    Initial Commit

---
+++ CalendarApp/CalendarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAppApp.swift
+//  CalendarApp
+//
+//  Created by John Reichel on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct CalendarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
